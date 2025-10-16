f8d3fa7a41f1988523415360f418e2c88a09b4ca
stavasilevv-hub <stavasilevv@gmail.com>
2025-10-16 21:48:19 +0300

    Add functions

---
+++ functions.ts
@@ -0,0 +1,42 @@
+// Functions
+function displayPersonalData(name: string, age: number): string {
+  return `${name} is ${age} years old`;
+}
+
+// console.log(displayPersonalData("Ivan", 20));
+
+const personalData: string = displayPersonalData("Peter", 25);
+// console.log(personalData);
+
+
+//void function - ne vrushta stoinost
+
+
+function devideNums(numOne: number, numTwo: number): number {
+  return numOne / numTwo;
+}
+
+devideNums(10, 5);
+devideNums(50, 5);
+devideNums(100, 5);
+
+// arrow function
+const sumNums = (a: number, b: number) => a+b;
+sumNums(3, 2);
+
+// optional parameters
+function greeting(name: string, age?:number) {
+  if (age) {
+    return `${name} is ${age} years old`;
+  }
+  return `Hi I'm ${name}`;
+}
+// console.log(greeting("Todor"));
+// console.log(greeting("Trifon",20));
+
+// default parameters
+function substractNums(numOne: number = 10, numTwo: number = 5){
+  console.log(numOne - numTwo);
+}
+substractNums(50);
+substractNums(50, 30);
